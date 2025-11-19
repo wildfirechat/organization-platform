5204c129dcc7a98d9deb32b02cec116686053549
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2025-11-19 16:03:51 +0800

    搜索名字支持模糊搜索

--- organization-server/src/main/java/cn/wildfirechat/org/jpa/EmployeeEntityRepository.java
+++ organization-server/src/main/java/cn/wildfirechat/org/jpa/EmployeeEntityRepository.java
@@ -15,22 +15,18 @@ public interface EmployeeEntityRepository extends CrudRepository<EmployeeEntity,
     @Query(value = "select * from t_employee where employee_id in (select distinct(employee_id) from t_relationship where organization_id = ?1 and bottom = true )", nativeQuery = true)
     List<EmployeeEntity> findByOrganizationId(int organizationId);
 
-    @Query(value = "select * from t_employee where (name like %?1% or mobile = ?1) and employee_id in (select distinct(employee_id) from t_relationship where organization_id = ?2 ) ",
-            countQuery = "select count(*) from t_employee where (name like %?1% or mobile = ?1) and employee_id in (select distinct(employee_id) from t_relationship where organization_id = ?2 )",
+    @Query(value = "select * from t_employee where (name like CONCAT('%',?1,'%') or mobile = ?1) and employee_id in (select distinct(employee_id) from t_relationship where organization_id = ?2 ) ",
+            countQuery = "select count(*) from t_employee where (name like  CONCAT('%',?1,'%') or mobile = ?1) and employee_id in (select distinct(employee_id) from t_relationship where organization_id = ?2 )",
             nativeQuery = true)
     Page<EmployeeEntity> searchByKeywordAndOrganization(String keyword, int organizationId, Pageable pageable);
 
-    @Query(value = "select * from t_employee where (name like %?1% or mobile = ?1) and employee_id in (select distinct(employee_id) from t_relationship where organization_id = ?2 and bottom = true ) ",
-            countQuery = "select count(*) from t_employee where (name like %?1% or mobile = ?1) and employee_id in (select distinct(employee_id) from t_relationship where organization_id = ?2 and bottom = true )",
+    @Query(value = "select * from t_employee where (name LIKE CONCAT('%',?1,'%') or mobile = ?1) and employee_id in (select distinct(employee_id) from t_relationship where organization_id = ?2 and bottom = true ) ",
+            countQuery = "select count(*) from t_employee where (name like  CONCAT('%',?1,'%') or mobile = ?1) and employee_id in (select distinct(employee_id) from t_relationship where organization_id = ?2 and bottom = true )",
             nativeQuery = true)
     Page<EmployeeEntity> searchByKeywordAndOrganizationRoot(String keyword, int organizationId, Pageable pageable);
-
-    //这里搜索多个字段有问题，待解决
-//    @Query(value = "select * from t_employee where name like %?1% or title like %?1% or mobile = ?1 or email = ?1",
-//            countQuery = "select count(*) from t_employee where name like %?1% or title like %?1% or mobile = ?1 or email = ?1",
-//            nativeQuery = true)
-    @Query(value = "select * from t_employee where name like %?1% or mobile = ?1",
-            countQuery = "select count(*) from t_employee where name like %?1% or mobile = ?1",
+    
+    @Query(value = "select * from t_employee where name like  CONCAT('%',?1,'%') or mobile = ?1",
+            countQuery = "select count(*) from t_employee where name like  CONCAT('%',?1,'%') or mobile = ?1",
             nativeQuery = true)
     Page<EmployeeEntity> searchByKeyword(String keyword, Pageable pageable);
 
